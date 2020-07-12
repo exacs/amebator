34dd6450e69e29f95d6a2f19c5e723cd56165892
Carlos Saito <carlos@saito.style>
2020-07-12 11:13:05 +0200

    Basic changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="App-header">
+        <div style={{padding: '32px'}}>
+          Welcome to the amebator
+        </div>
+        <canvas width="500" height="500" style={{border: '1px solid white'}}>Here is the Canvas</canvas>
+
+      </main>
     </div>
   );
 }
