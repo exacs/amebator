42b6d01521cd8db03fc3b13f6b2ca010168400e1
Carlos Saito <carlos@saito.style>
2020-07-15 10:39:47 +0200

    React router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React from "react";
-import Amebator from "./components/Amebator";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import "./App.css";
+import Amebator from "./components/Amebator"
 
-function App() {
-  return <Amebator />;
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Amebator v0</Link>
+            </li>
+            <li>
+              <Link to="/horizon">Amebator with horizon</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/horizon">
+            <Horizon />
+          </Route>
+          <Route path="/">
+            <Amebator />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+function Horizon() {
+  return <h2>Amebator with horizon</h2>;
+}
