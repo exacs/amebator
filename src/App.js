2cb6f247a536e31c7889d9e7fdd64d1e48a903ef
Carlos Saito <carlos@saito.style>
2020-07-16 17:18:49 +0200

    Style App

--- src/App.js
+++ src/App.js
@@ -34,16 +34,16 @@ export default function App() {
         </nav>
       </div>
 
-      {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/2">
-          <Horizon />
-        </Route>
-        <Route path="/">
-          <Amebator />
-        </Route>
-      </Switch>
+      <main className="App-body">
+        <Switch>
+          <Route path="/2">
+            <Horizon />
+          </Route>
+          <Route path="/">
+            <Amebator />
+          </Route>
+        </Switch>
+      </main>
     </Router>
   );
 }
