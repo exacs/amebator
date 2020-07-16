670a5d9c015f4c65f386bb1b7ad58b43fbfb45a9
Carlos Saito <carlos@saito.style>
2020-07-16 17:08:37 +0200

    Add links

--- src/App.js
+++ src/App.js
@@ -3,41 +3,51 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link as DefaultLink,
+  useRouteMatch
 } from "react-router-dom";
 import "./App.css";
 import Amebator from "./components/Amebator"
 
+function Link ({ children, to }) {
+  const match = useRouteMatch({
+    path: to,
+    exact: true
+  })
+  const className = [
+    'App-nav-link',
+    match ? 'App-nav-link-selected' : ''
+  ].join(' ')
+
+  return (
+    <DefaultLink to={to} className={className}>{children}</DefaultLink>
+  )
+}
+
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Amebator v0</Link>
-            </li>
-            <li>
-              <Link to="/horizon">Amebator with horizon</Link>
-            </li>
-          </ul>
+      <div className="App-nav-container">
+        <nav className="App-nav">
+          <Link to="/">Amebator classic</Link>
+          <Link to="/2">Amebator II</Link>
         </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/horizon">
-            <Horizon />
-          </Route>
-          <Route path="/">
-            <Amebator />
-          </Route>
-        </Switch>
       </div>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/2">
+          <Horizon />
+        </Route>
+        <Route path="/">
+          <Amebator />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
 function Horizon() {
-  return <h2>Amebator with horizon</h2>;
+  return <h2>Amebator with centers and tangents</h2>;
 }
