8815976e6b7341b0b2f0a83781364dfa27d08b9a
Carlos Saito <carlos@saito.style>
2020-07-22 17:45:39 +0200

    Delete "Amebator" component

--- src/app.js
+++ src/app.js
@@ -1,15 +1,38 @@
 import React from "react";
+import Canvas from "./components/canvas";
+import DebugCanvas from "./components/debug-canvas";
+import CirclesEditor from "./components/circles-editor";
+import PlusMinus from "./components/plus-minus";
+import useAmebaState from "./hooks/use-ameba-state";
 import "./app.css";
-import Amebator from "./components/amebator";
 
 export default function App() {
-  return (
-    <div>
-      <div className="App-nav-container"></div>
+  const {
+    data,
+    setSize,
+    setCircleRadius,
+    setCircleCenter,
+    setTangentRadius,
+  } = useAmebaState(2);
 
-      <main className="App-body">
-        <Amebator />
-      </main>
-    </div>
+  return (
+    <main className="App-body">
+      <div className="Amebator-canvas-container">
+        <DebugCanvas data={data} />
+        <Canvas data={data} />
+        <CirclesEditor
+          data={data}
+          moveCenter={setCircleCenter}
+          changeRadius={setCircleRadius}
+          changeTangentRadius={setTangentRadius}
+        />
+      </div>
+      <div className="Amebator-control">
+        <PlusMinus
+          amount={data.circles.length}
+          onSetAmount={(amount) => setSize(amount)}
+        />
+      </div>
+    </main>
   );
 }
